99ba7d60ea17f1c7d49a5c58fb708d174aff34b3
cxdtotsj <cxdtotsj@163.com>
2018-11-02 17:47:22 +0800

    unittest框架用例编写规范

--- testpy/request.py
+++ testpy/request.py
@@ -1,8 +1,15 @@
 import requests
+import json
 
-url = "https://dt-dev.arctron.cn/api/user/login"
-data = {"email":"admin@admin",
-        "password":"abc123"}
+# url = "https://dt-dev.arctron.cn/api/user/login"
+# data = {"email":"admin@admin",
+#         "password":"abc123"}
 
-res = requests.post(url,data).json()
-print(res)
+# res = requests.post(url,data).json()
+# print(res)
+
+r = requests.get('https://api.github.com/events')
+print(r.content)
+
+dict_json = json.loads(r.content)
+print(dict_json)
