f455e5821db3e910c1bec57d8f49af3871eb20c5
cxdtotsj <cxdtotsj@163.com>
2020-04-22 10:08:52 +0800

    生成器相关

--- python/learn_tricks/test_iter.py
+++ python/learn_tricks/test_iter.py
@@ -49,26 +49,93 @@ print(next(itertor))
 
 
 # 有限次数的迭代器
-class BoundsRepeater:
-    def __init__(self, value, max_repeats):
-        self.value = value
-        self.max_repeats = max_repeats
-        self.count = 0
+# class BoundsRepeater:
+#     def __init__(self, value, max_repeats):
+#         self.value = value
+#         self.max_repeats = max_repeats
+#         self.count = 0
 
-    def __iter__(self):
-        return self
+#     def __iter__(self):
+#         return self
     
-    def __next__(self):
-        if self.count >= self.max_repeats:
-            raise StopIteration
-        self.count += 1
-        return self.value
+#     def __next__(self):
+#         if self.count >= self.max_repeats:
+#             raise StopIteration
+#         self.count += 1
+#         return self.value
 
-repeater = BoundsRepeater("Yes", 3)
-# for item in repeater:
-#     print(item)
-itertor = iter(repeater)
-print(next(itertor))
-print(next(itertor))
-print(next(itertor))
-print(next(itertor))
+# repeater = BoundsRepeater("Yes", 3)
+# # for item in repeater:
+# #     print(item)
+# itertor = iter(repeater)
+# print(next(itertor))
+# print(next(itertor))
+# print(next(itertor))
+# print(next(itertor))
+
+
+# 使用生成器改写
+def repeater_generator(value):
+    while True:
+        yield value
+
+# for i in repeater_generator("Hello")
+rg = repeater_generator("Hello")
+print(rg)
+print(next(rg))
+print(next(rg))
+
+
+# 停下来的生成器
+def repeater_three_times(value):
+    yield value
+    yield value
+    yield value
+
+# for x in repeater_three_times("World"):
+#     print(x)
+
+# rg = repeater_three_times("World")
+# print(next(rg))
+# print(next(rg))
+# print(next(rg))
+# print(next(rg))
+
+
+def bounded_repeater(value, max_repeats):
+    # 原始方法
+    # count = 0
+    # while True:
+    #     if count >= max_repeats:
+    #         return
+    #     count += 1
+    #     yield value
+    for i in range(max_repeats):
+        yield value
+
+
+# 列表解析式
+genexpr = ('Hello' for i in range(3))
+
+
+# 迭代器链，推荐的设计方法，类似于管道
+def integers():
+    for i in range(1,9):
+        yield i
+
+inter = integers()
+print(list(inter))
+
+def squared(seq):
+    for i in seq:
+        yield i * i
+
+squ = squared(integers())
+print(list(squ))
+
+def negated(squ):
+    for i in squ:
+        yield -i
+
+neg = negated(squared(integers()))
+print(list(neg))
